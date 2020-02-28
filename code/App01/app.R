d6201957a8f074255bb7b14dfa725ad2e806e1d1
Bryce Askey <bryceaskey@gmail.com>
2020-02-28 13:39:07 -0500

    Completed lesson 3 in RShiny tutorial

--- code/App01/app.R
+++ code/App01/app.R
@@ -2,79 +2,19 @@ library(shiny)
 
 # Define UI ----
 ui <- fluidPage(
-  titlePanel("Basic widgets"),
-  
-  fluidRow(
-    
-    column(3,
-           h3("Buttons"),
-           actionButton("action", "Action"),
-           br(),
-           br(), 
-           submitButton("Submit")),
-    
-    column(3,
-           h3("Single checkbox"),
-           checkboxInput("checkbox", "Choice A", value = TRUE)),
-    
-    column(3, 
-           checkboxGroupInput("checkGroup", 
-                              h3("Checkbox group"), 
-                              choices = list("Choice 1" = 1, 
-                                             "Choice 2" = 2, 
-                                             "Choice 3" = 3),
-                              selected = 1)),
-    
-    column(3, 
-           dateInput("date", 
-                     h3("Date input"), 
-                     value = "2014-01-01"))   
-  ),
-  
-  fluidRow(
-    
-    column(3,
-           dateRangeInput("dates", h3("Date range"))),
-    
-    column(3,
-           fileInput("file", h3("File input"))),
-    
-    column(3, 
-           h3("Help text"),
-           helpText("Note: help text isn't a true widget,", 
-                    "but it provides an easy way to add text to",
-                    "accompany other widgets.")),
-    
-    column(3, 
-           numericInput("num", 
-                        h3("Numeric input"), 
-                        value = 1))   
-  ),
-  
-  fluidRow(
-    
-    column(3,
-           radioButtons("radio", h3("Radio buttons"),
-                        choices = list("Choice 1" = 1, "Choice 2" = 2,
-                                       "Choice 3" = 3),selected = 1)),
-    
-    column(3,
-           selectInput("select", h3("Select box"), 
-                       choices = list("Choice 1" = 1, "Choice 2" = 2,
-                                      "Choice 3" = 3), selected = 1)),
-    
-    column(3, 
-           sliderInput("slider1", h3("Sliders"),
-                       min = 0, max = 100, value = 50),
-           sliderInput("slider2", "",
-                       min = 0, max = 100, value = c(25, 75))
+  titlePanel("censusVis"),
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with information form the 2010 US Census."),
+      selectInput("var", h3("Choose a variable to display"),
+                  choices=list("Percent White"=1, "Percent Black"=2, "Percent Hispanic"=3, "Percent Asian"=4),
+                  selected=1),
+      sliderInput("range", h3("Range of interest"),
+                  min=0, max=100, value=c(0, 100))
     ),
-    
-    column(3, 
-           textInput("text", h3("Text input"), 
-                     value = "Enter text..."))   
+    mainPanel(
+    )
   )
-  
 )
 
 # Define server logic ----
