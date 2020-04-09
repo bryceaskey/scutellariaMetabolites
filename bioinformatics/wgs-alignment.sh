3130db071354575ef89d05586c74695ddba1073c
Bryce Askey <bryceaskey@gmail.com>
2020-04-09 11:00:51 -0400

    Increased amount of allocated memory

--- bioinformatics/wgs-alignment.sh
+++ bioinformatics/wgs-alignment.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=END,FAIL            # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=braskey@ufl.edu     # Where to send mail	
 #SBATCH --ntasks=1                      # Run on a single CPU
-#SBATCH --mem=1gb                       # Job memory request
+#SBATCH --mem=4gb                       # Job memory request
 #SBATCH --time=720                      # Time limit hrs:min:sec
 #SBATCH --output=wgs-alignment_%j.log   # Standard output and error log
 
