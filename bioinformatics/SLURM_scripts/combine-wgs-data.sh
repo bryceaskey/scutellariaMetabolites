51663080fc25196e0cc59d442fc74600c01f5fb4
Bryce Askey <bryceaskey@gmail.com>
2020-04-14 12:28:10 -0400

    Finalized script to merge wgs data into 1 file

--- bioinformatics/SLURM_scripts/combine-wgs-data.sh
+++ bioinformatics/SLURM_scripts/combine-wgs-data.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=END,FAIL               # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=braskey@ufl.edu        # Where to send mail	
 #SBATCH --ntasks=1                         # Run on a single CPU
-#SBATCH --mem=1gb                          # Job memory request
+#SBATCH --mem=4gb                          # Job memory request
 #SBATCH --time=12:00:00                    # Time limit hrs:min:sec
 #SBATCH --output=combine-wgs-data_%j.log   # Standard output and error log
 
@@ -11,4 +11,4 @@ pwd; hostname; date
 
 module load python/3.8
 
-python /ufrc/lee/braskey/combine-wgs-data.py
+python /ufrc/lee/braskey/Python_scripts/combine-wgs-data.py
