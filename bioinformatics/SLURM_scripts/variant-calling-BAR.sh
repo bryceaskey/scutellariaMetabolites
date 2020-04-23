c3acf012289aeab2f9b986e6292d8a897e418465
Bryce Askey <bryceaskey@gmail.com>
2020-04-23 14:01:17 -0400

    Fixed issue with module loading

--- bioinformatics/SLURM_scripts/variant-calling-BAR.sh
+++ bioinformatics/SLURM_scripts/variant-calling-BAR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=variant-calling-ALT      # Job name
+#SBATCH --job-name=variant-calling-BAR      # Job name
 #SBATCH --account=lee                       # Account name
 #SBATCH --qos=lee                           # QOS name
 #SBATCH --mail-type=END,FAIL                # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -7,19 +7,19 @@
 #SBATCH --ntasks=1                          # Run on a single CPU
 #SBATCH --mem=4gb                           # Job memory request
 #SBATCH --time=120:00:00                    # Time limit hrs:min:sec
-#SBATCH --output=variant-calling-ALT_%j.log # Standard output and error log
+#SBATCH --output=variant-calling-BAR_%j.log # Standard output and error log
 
 pwd; hostname; date
 
 module load bcftools/1.10.2
 
-echo "Calling variants for aligned altissima genomic data"
+echo "Calling variants for aligned barbata genomic data"
 
 ref=/ufrc/lee/braskey/Data/ASM577160v1/ncbi_dataset/data/GCA_005771605.1/GCA005771605.fa
-sorted_aln=/ufrc/lee/braskey/Data/WGS/ALT_aln_sorted.bam
+sorted_aln=/ufrc/lee/braskey/Data/WGS/BAR_aln_sorted.bam
 output_dir=/ufrc/lee/braskey/Data/WGS/
 
-bcftools mpileup -Ov -f ${ref} ${sorted_aln} | bcftools call -c -V indels -o ${output_dir}ALT_aln.vcf
-bcftools filter -Ov -i 'DP>7' ${output_dir}ALT_aln.vcf -o ${output_dir}ALT_aln_filtered.vcf
-bcftools view ${output_dir}ALT_aln_filtered.vcf -Oz -o ${output_dir}ALT_aln_filtered.vcf.gz
-bcftools index ${output_dir}ALT_aln_filtered.vcf.gz
+bcftools mpileup -Ov -f ${ref} ${sorted_aln} | bcftools call -c -V indels -o ${output_dir}BAR_aln.vcf
+bcftools filter -Ov -i 'DP>7' ${output_dir}BAR_aln.vcf -o ${output_dir}BAR_aln_filtered.vcf
+bcftools view ${output_dir}BAR_aln_filtered.vcf -Oz -o ${output_dir}BAR_aln_filtered.vcf.gz
+bcftools index ${output_dir}BAR_aln_filtered.vcf.gz
--- bioinformatics/SLURM_scripts/variant-calling-HAV.sh
+++ bioinformatics/SLURM_scripts/variant-calling-HAV.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=variant-calling-ALT      # Job name
+#SBATCH --job-name=variant-calling-HAV      # Job name
 #SBATCH --account=lee                       # Account name
 #SBATCH --qos=lee                           # QOS name
 #SBATCH --mail-type=END,FAIL                # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -7,19 +7,19 @@
 #SBATCH --ntasks=1                          # Run on a single CPU
 #SBATCH --mem=4gb                           # Job memory request
 #SBATCH --time=120:00:00                    # Time limit hrs:min:sec
-#SBATCH --output=variant-calling-ALT_%j.log # Standard output and error log
+#SBATCH --output=variant-calling-HAV_%j.log # Standard output and error log
 
 pwd; hostname; date
 
 module load bcftools/1.10.2
 
-echo "Calling variants for aligned altissima genomic data"
+echo "Calling variants for aligned havanesis genomic data"
 
 ref=/ufrc/lee/braskey/Data/ASM577160v1/ncbi_dataset/data/GCA_005771605.1/GCA005771605.fa
-sorted_aln=/ufrc/lee/braskey/Data/WGS/ALT_aln_sorted.bam
+sorted_aln=/ufrc/lee/braskey/Data/WGS/HAV_aln_sorted.bam
 output_dir=/ufrc/lee/braskey/Data/WGS/
 
-bcftools mpileup -Ov -f ${ref} ${sorted_aln} | bcftools call -c -V indels -o ${output_dir}ALT_aln.vcf
-bcftools filter -Ov -i 'DP>7' ${output_dir}ALT_aln.vcf -o ${output_dir}ALT_aln_filtered.vcf
-bcftools view ${output_dir}ALT_aln_filtered.vcf -Oz -o ${output_dir}ALT_aln_filtered.vcf.gz
-bcftools index ${output_dir}ALT_aln_filtered.vcf.gz
+bcftools mpileup -Ov -f ${ref} ${sorted_aln} | bcftools call -c -V indels -o ${output_dir}HAV_aln.vcf
+bcftools filter -Ov -i 'DP>7' ${output_dir}HAV_aln.vcf -o ${output_dir}HAV_aln_filtered.vcf
+bcftools view ${output_dir}HAV_aln_filtered.vcf -Oz -o ${output_dir}HAV_aln_filtered.vcf.gz
+bcftools index ${output_dir}HAV_aln_filtered.vcf.gz
