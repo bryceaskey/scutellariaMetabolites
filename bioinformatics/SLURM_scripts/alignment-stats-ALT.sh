a33cf5e6f988152b30602b66856d1c2ddc9b703d
Bryce Askey <bryceaskey@gmail.com>
2020-04-30 13:20:29 -0400

    Created scripts for generating alignment and variant calling statistics

---
+++ bioinformatics/SLURM_scripts/alignment-stats-ALT.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=alignment-stats-ALT      # Job name
+#SBATCH --account=lee                       # Account name
+#SBATCH --qos=lee                           # QOS name
+#SBATCH --mail-type=END,FAIL                # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=braskey@ufl.edu         # Where to send mail	
+#SBATCH --ntasks=1                          # Run on a single CPU
+#SBATCH --mem=4gb                           # Job memory request
+#SBATCH --time=120:00:00                    # Time limit hrs:min:sec
+#SBATCH --output=alignment-stats-ALT_%j.log # Standard output and error log
+
+pwd; hostname; date
+
+module load samtools/1.10 bcftools/1.10.2
+
+wgs_data=/ufrc/lee/braskey/Data/WGS/
+
+samtools stats ${wgs_data}ALT_aln_sorted.bam
+bcftools stats ${wgs_data}ALT_aln_filtered.vcf
