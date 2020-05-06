74617f03fc7525aed4dbd5305a2fd4b9b56b0993
Bryce <bryceaskey@gmail.com>
2020-05-06 15:02:42 -0400

    Created SLURM scripts for WGS alignment with Novoalign

---
+++ bioinformatics/SLURM_scripts/wgs-alignment-novo-ALT.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=wgs-alignment-novo-ALT        # Job name
+#SBATCH --account=lee                            # Account name
+#SBATCH --qos=lee                                # QOS name
+#SBATCH --mail-type=END,FAIL                     # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=braskey@ufl.edu              # Where to send mail	
+#SBATCH --ntasks=1                               # Run on a single CPU
+#SBATCH --mem=4gb                                # Job memory request
+#SBATCH --time=120:00:00                         # Time limit hrs:min:sec
+#SBATCH --output=wgs-alignment-novo-ALT_%j.log   # Standard output and error log
+
+pwd; hostname; date
+
+module load novoalign/3.00.02 samtools/1.10
+
+echo "Aligning altissima WGS data to reference genome"
+
+ref=/ufrc/lee/braskey/Data/ASM577160v1/ncbi_dataset/data/GCA_005771605.1/
+wgs=/ufrc/lee/braskey/Data/WGS/
+
+gunzip -c ${wgs}ALT_1.fq.gz > ${wgs}ALT_1.fq
+gunzip -c ${wgs}ALT_2.fq.gz > ${wgs}ALT_2.fq
+
+novoindex ${ref}GCA005771605.nix ${ref}GCA005771605.fa
+novoalign -d ${ref}GCA005771605.nix -f  ${wgs}ALT_1.fq ${wgs}ALT_2.fq -o SAM > ${output_dir}ALT_novoaln.sam
+samtools view -bS ${wgs}ALT_novoaln.sam > ${wgs}ALT_novoaln.bam
+samtools sort -O bam -o ${wgs}ALT_novoaln_sorted.bam ${wgs}ALT_novoaln.bam
---
+++ bioinformatics/SLURM_scripts/wgs-alignment-novo-BAR.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=wgs-alignment-novo-BAR        # Job name
+#SBATCH --account=lee                            # Account name
+#SBATCH --qos=lee                                # QOS name
+#SBATCH --mail-type=END,FAIL                     # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=braskey@ufl.edu              # Where to send mail	
+#SBATCH --ntasks=1                               # Run on a single CPU
+#SBATCH --mem=4gb                                # Job memory request
+#SBATCH --time=120:00:00                         # Time limit hrs:min:sec
+#SBATCH --output=wgs-alignment-novo-BAR_%j.log   # Standard output and error log
+
+pwd; hostname; date
+
+module load novoalign/3.00.02 samtools/1.10
+
+echo "Aligning barbata WGS data to reference genome"
+
+ref=/ufrc/lee/braskey/Data/ASM577160v1/ncbi_dataset/data/GCA_005771605.1/
+wgs=/ufrc/lee/braskey/Data/WGS/
+
+gunzip -c ${wgs}BAR_1.fq.gz > ${wgs}BAR_1.fq
+gunzip -c ${wgs}BAR_2.fq.gz > ${wgs}BAR_2.fq
+
+novoindex ${ref}GCA005771605.nix ${ref}GCA005771605.fa
+novoalign -d ${ref}GCA005771605.nix -f  ${wgs}BAR_1.fq ${wgs}BAR_2.fq -o SAM > ${output_dir}BAR_novoaln.sam
+samtools view -bS ${wgs}BAR_novoaln.sam > ${wgs}BAR_novoaln.bam
+samtools sort -O bam -o ${wgs}BAR_novoaln_sorted.bam ${wgs}BAR_novoaln.bam
