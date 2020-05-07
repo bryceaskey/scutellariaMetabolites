4e2b5cf8a850a3aba548922b1d56a738dbe584a7
Bryce <bryceaskey@gmail.com>
2020-05-06 23:06:57 -0400

    Fixed error in novoalign script

--- bioinformatics/SLURM_scripts/wgs-novoalign-HAV.sh
+++ bioinformatics/SLURM_scripts/wgs-novoalign-HAV.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=wgs-alignment-novo-BAR        # Job name
+#SBATCH --job-name=wgs-novoalign-HAV             # Job name
 #SBATCH --account=lee                            # Account name
 #SBATCH --qos=lee                                # QOS name
 #SBATCH --mail-type=END,FAIL                     # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -7,21 +7,21 @@
 #SBATCH --ntasks=1                               # Run on a single CPU
 #SBATCH --mem=4gb                                # Job memory request
 #SBATCH --time=120:00:00                         # Time limit hrs:min:sec
-#SBATCH --output=wgs-alignment-novo-BAR_%j.log   # Standard output and error log
+#SBATCH --output=wgs-novoalign-HAV_%j.log        # Standard output and error log
 
 pwd; hostname; date
 
 module load novoalign/3.00.02 samtools/1.10
 
-echo "Aligning barbata WGS data to reference genome"
+echo "Aligning havanesis WGS data to reference genome"
 
 ref=/ufrc/lee/braskey/Data/ASM577160v1/ncbi_dataset/data/GCA_005771605.1/
 wgs=/ufrc/lee/braskey/Data/WGS/
 
-gunzip -c ${wgs}BAR_1.fq.gz > ${wgs}BAR_1.fq
-gunzip -c ${wgs}BAR_2.fq.gz > ${wgs}BAR_2.fq
+gunzip -c ${wgs}HAV_1.fq.gz > ${wgs}HAV_1.fq
+gunzip -c ${wgs}HAV_2.fq.gz > ${wgs}HAV_2.fq
 
 novoindex ${ref}GCA005771605.nix ${ref}GCA005771605.fa
-novoalign -d ${ref}GCA005771605.nix -f  ${wgs}BAR_1.fq ${wgs}BAR_2.fq -o SAM > ${output_dir}BAR_novoaln.sam
-samtools view -bS ${wgs}BAR_novoaln.sam > ${wgs}BAR_novoaln.bam
-samtools sort -O bam -o ${wgs}BAR_novoaln_sorted.bam ${wgs}BAR_novoaln.bam
+novoalign -d ${ref}GCA005771605.nix -f  ${wgs}HAV_1.fq ${wgs}HAV_2.fq -o SAM > ${wgs}HAV_novoaln.sam
+samtools view -bS ${wgs}HAV_novoaln.sam > ${wgs}HAV_novoaln.bam
+samtools sort -O bam -o ${wgs}HAV_novoaln_sorted.bam ${wgs}HAV_novoaln.bam
--- bioinformatics/SLURM_scripts/wgs-novoalign-RAC.sh
+++ bioinformatics/SLURM_scripts/wgs-novoalign-RAC.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=wgs-alignment-novo-RAC        # Job name
+#SBATCH --job-name=wgs-novoalign-RAC             # Job name
 #SBATCH --account=lee                            # Account name
 #SBATCH --qos=lee                                # QOS name
 #SBATCH --mail-type=END,FAIL                     # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1                               # Run on a single CPU
 #SBATCH --mem=4gb                                # Job memory request
 #SBATCH --time=120:00:00                         # Time limit hrs:min:sec
-#SBATCH --output=wgs-alignment-novo-RAC_%j.log   # Standard output and error log
+#SBATCH --output=wgs-novoalign-RAC_%j.log        # Standard output and error log
 
 pwd; hostname; date
 
@@ -22,6 +22,6 @@ gunzip -c ${wgs}RAC_1.fq.gz > ${wgs}RAC_1.fq
 gunzip -c ${wgs}RAC_2.fq.gz > ${wgs}RAC_2.fq
 
 novoindex ${ref}GCA005771605.nix ${ref}GCA005771605.fa
-novoalign -d ${ref}GCA005771605.nix -f  ${wgs}RAC_1.fq ${wgs}RAC_2.fq -o SAM > ${output_dir}RAC_novoaln.sam
+novoalign -d ${ref}GCA005771605.nix -f  ${wgs}RAC_1.fq ${wgs}RAC_2.fq -o SAM > ${wgs}RAC_novoaln.sam
 samtools view -bS ${wgs}RAC_novoaln.sam > ${wgs}RAC_novoaln.bam
 samtools sort -O bam -o ${wgs}RAC_novoaln_sorted.bam ${wgs}RAC_novoaln.bam
