95b4c7355713740179bfc5587fc2e2cc6bd0324e
Bryce Askey <bryceaskey@gmail.com>
2020-05-13 11:33:43 -0400

    Added indel calling with base and mapping quality thresholds

--- bioinformatics/SLURM_scripts/variant-calling-RAC.sh
+++ bioinformatics/SLURM_scripts/variant-calling-RAC.sh
@@ -19,7 +19,7 @@ ref=/ufrc/lee/braskey/Data/ASM577160v1/ncbi_dataset/data/GCA_005771605.1/GCA0057
 sorted_aln=/ufrc/lee/braskey/Data/WGS/bwa/RAC_aln_sorted.bam
 output_dir=/ufrc/lee/braskey/Data/WGS/bwa/
 
-bcftools mpileup -Ov -f ${ref} ${sorted_aln} | bcftools call -c -V indels -o ${output_dir}RAC_aln.vcf
-bcftools filter -Ov -i 'DP>7' ${output_dir}RAC_aln.vcf -o ${output_dir}RAC_aln_filtered.vcf
-bcftools view ${output_dir}RAC_aln_filtered.vcf -Oz -o ${output_dir}RAC_aln_filtered.vcf.gz
-bcftools index ${output_dir}RAC_aln_filtered.vcf.gz
+bcftools mpileup -Ov -Q 20 -f ${ref} ${sorted_aln} | bcftools call -c -o ${output_dir}RAC_aln.vcf
+bcftools filter -Ov -i '%QUAL>20' ${output_dir}RAC_aln.vcf -o ${output_dir}RAC_aln_flt.vcf
+bcftools index ${output_dir}RAC_aln_flt.vcf
+bcftools stats ${output_dir}RAC_aln_flt.vcf
