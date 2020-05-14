ee3933847348fbd386580373a76f73844cab2025
Bryce Askey <bryceaskey@gmail.com>
2020-05-13 23:56:41 -0400

    Developed R method for visualization of variants

---
+++ bioinformatics/SLURM_scripts/variant-vis-ALT.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=variant-vis-ALT          # Job name
+#SBATCH --account=lee                       # Account name
+#SBATCH --qos=lee                           # QOS name
+#SBATCH --mail-type=END,FAIL                # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=braskey@ufl.edu         # Where to send mail	
+#SBATCH --ntasks=1                          # Run on a single CPU
+#SBATCH --mem=16gb                          # Job memory request
+#SBATCH --time=120:00:00                    # Time limit hrs:min:sec
+#SBATCH --output=variant-vis-ALT_%j.log     # Standard output and error log
+
+pwd; hostname; date
+
+module load R/3.6
+
+echo "Generating visualization of variants for altissima"
+
+Rscript /ufrc/lee/braskey/R_scripts/variant-vis-ALT.R
