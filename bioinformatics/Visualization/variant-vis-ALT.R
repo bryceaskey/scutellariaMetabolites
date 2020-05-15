0b71ec85a57f1944962697c9bf2a51be35ada2ce
Bryce Askey <bryceaskey@gmail.com>
2020-05-15 09:37:11 -0400

    Generated figures for RAC with v1 of visualization method

--- bioinformatics/Visualization/variant-vis-ALT.R
+++ bioinformatics/Visualization/variant-vis-ALT.R
@@ -6,12 +6,14 @@ variant_path <- "/ufrc/lee/braskey/Data/WGS/bwa/"
 ref_path <- "/ufrc/lee/braskey/Data/ASM577160v1/ncbi_dataset/data/GCA_005771605.1/"
 output_path <- "/ufrc/lee/braskey/Figures/"
 
-pdf(paste(output_path, species, "_variantPlots.pdf", sep=""))
 for (chrNum in 1:9) {
   vcf <- read.vcfR(paste(variant_path, species, "_aln_flt_chr", chrNum, ".vcf.gz", sep=""))
   ref <- ape::read.dna(paste(ref_path, "chr", chrNum, ".fa", sep=""), format="fasta")
   chrom <- create.chromR(name=paste(species, "chromosome", chrNum), vcf=vcf, seq=ref)
   chrom <- proc.chromR(chrom, verbose=TRUE)
-  chromoqc(chrom, dp.alpha=20)
+ 
+  png(paste(output_path, species, "_chr", chrNum, "_variantPlot.png", sep=""),
+      height=2000, width=3000)
+  chromoqc(chrom, dp.alpha=150)
+  dev.off()
 }
-dev.off()
