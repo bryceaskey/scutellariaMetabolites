e575a7e8d5a09f06c4b9b78fcb5498fa68292331
Bryce Askey <bryceaskey@gmail.com>
2020-05-15 09:37:11 -0400

    Generated figures for RAC with v1 of visualization method

--- bioinformatics/SLURM_scripts/variant-vis-ALT.sh
+++ bioinformatics/SLURM_scripts/variant-vis-ALT.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=END,FAIL                # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=braskey@ufl.edu         # Where to send mail	
 #SBATCH --ntasks=1                          # Run on a single CPU
-#SBATCH --mem=16gb                          # Job memory request
+#SBATCH --mem=48gb                          # Job memory request
 #SBATCH --time=120:00:00                    # Time limit hrs:min:sec
 #SBATCH --output=variant-vis-ALT_%j.log     # Standard output and error log
 
