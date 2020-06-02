be8231e632aff72dc4495ec0c0a64a0e021fd0dd
Bryce Askey <bryceaskey@gmail.com>
2020-06-02 10:49:46 -0400

    Fixed error in function call

--- bioinformatics/SLURM_scripts/RNAseq-STAR-RAC-root3.sh
+++ bioinformatics/SLURM_scripts/RNAseq-STAR-RAC-root3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=RNAseq-STAR-RAC-leaf1       # Job name
+#SBATCH --job-name=RNAseq-STAR-RAC-root3       # Job name
 #SBATCH --account=lee                          # Account name
 #SBATCH --qos=lee                              # QOS name
 #SBATCH --mail-type=END,FAIL                   # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -7,16 +7,16 @@
 #SBATCH --ntasks=1                             # Run on a single CPU
 #SBATCH --mem=8gb                              # Job memory request
 #SBATCH --time=120:00:00                       # Time limit hrs:min:sec
-#SBATCH --output=RNAseq-STAR-RAC-leaf1_%j.log  # Standard output and error log
+#SBATCH --output=RNAseq-STAR-RAC-root3_%j.log  # Standard output and error log
 
 pwd; hostname; date
 
 module load star/2.7.3a
 
-echo "Mapping racemosa leaf1 RNAseq data to baicalensis reference genome"
+echo "Mapping racemosa root3 RNAseq data to baicalensis reference genome"
 
 sp=RAC
-rep=leaf1
+rep=root3
 ref=/ufrc/lee/braskey/Data/ASM577160v1/ncbi_dataset/data/GCA_005771605.1/copy_STAR/index/
 reads=/ufrc/lee/braskey/Data/RNAseq/
 aln=/ufrc/lee/braskey/Data/RNAseq/alignments/
