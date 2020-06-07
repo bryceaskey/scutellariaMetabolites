930cae41ac7568ae6c76714ceedade63579b75b8
Bryce Askey <bryceaskey@gmail.com>
2020-06-07 13:01:54 -0400

    Added log files for STAR RNAseq alignment

--- bioinformatics/SLURM_scripts/wgs-assemUnmapped-BAR.sh
+++ bioinformatics/SLURM_scripts/wgs-assemUnmapped-BAR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=wgs-assemUnmapped-ALT       # Job name
+#SBATCH --job-name=wgs-assemUnmapped-BAR       # Job name
 #SBATCH --account=lee                          # Account name
 #SBATCH --qos=lee                              # QOS name
 #SBATCH --mail-type=END,FAIL                   # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -10,15 +10,15 @@
 #SBATCH --ntasks-per-node=12
 #SBATCH --mem-per-cpu=4gb
 #SBATCH --time=740:00:00                       # Time limit hrs:min:sec
-#SBATCH --output=wgs-assemUnmapped-ALT_%j.log  # Standard output and error log
+#SBATCH --output=wgs-assemUnmapped-BAR_%j.log  # Standard output and error log
 
 pwd; hostname; date
 
 module load  intel/2018.1.163 boost/1.71.0 openmpi/3.1.0 sparsehash/2.0.3 abyss/2.1.0
 
-echo "Assembling unmapped altissima reads into contigs"
+echo "Assembling unmapped barbata reads into contigs"
 
-sp=ALT
+sp=BAR
 reads=/ufrc/lee/braskey/Data/WGS/bwa/unmapped/
 contigs=/ufrc/lee/braskey/Data/WGS/bwa/contigs/
 
